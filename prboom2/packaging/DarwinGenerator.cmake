cd7f26dd71034f4f16425d645f21d2c4bfdca729
Pedro Beirao <pedrocbeirao@gmail.com>
2026-07-25 18:55:26 +0100

    Use VCPKG_LIBRARY_LINKAGE as a cpack variable

--- prboom2/packaging/DarwinGenerator.cmake
+++ prboom2/packaging/DarwinGenerator.cmake
@@ -24,7 +24,7 @@ file(WRITE
   "xattr -dr com.apple.quarantine path/to/dsda-doom\n"
 )
 
-if(NOT VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+if(NOT CPACK_VCPKG_LIBRARY_LINKAGE STREQUAL "static")
   find_program(DYLIBBUNDLER_EXECUTABLE
     NAMES dylibbundler
     REQUIRED
